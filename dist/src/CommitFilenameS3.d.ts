37e64868741719891b63a7ff1b51baa43e96da1e
Matthew Forrester <matthew.forrester@speechmarks.com>
2017-11-04 12:20:02 +0000

    feat: Add quiet option and progress bar tweaks

--- dist/src/CommitFilenameS3.d.ts
+++ dist/src/CommitFilenameS3.d.ts
@@ -1,23 +1,16 @@
 import { Callback, Filename, S3BucketName } from './Types';
 import { Readable } from 'stronger-typed-streams';
 import { S3 } from 'aws-sdk';
-export declare abstract class EasyReadable<T> extends Readable<T> {
-    private readCount;
-    private buffer;
-    private ended;
-    private waiting;
-    constructor(opts?: {});
-    push(t: T | null, encoding?: string): boolean;
-    abstract __read(count: number, next: Callback<(null | T)[]>): any;
-    private processBuffer();
-    private doRead();
-    _read(count: any): void;
-}
-export default class S3CommitList extends EasyReadable<Filename> {
+export default class S3CommitList extends Readable<Filename> {
     private s3;
     private s3BucketName;
     private nextMarker;
+    private ended;
+    private waiting;
+    private count;
     constructor(s3: S3, s3BucketName: S3BucketName, opts?: {});
+    private doRead();
+    _read(count: any): void;
     map(c: S3.Object[]): Filename[];
     __read(count: any, next: Callback<(Filename | null)[]>): void;
 }
--- dist/src/process.d.ts
+++ dist/src/process.d.ts
@@ -1,8 +1,16 @@
 import { AbsoluteDirectoryPath } from './Types';
-export declare function push(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): void;
+export declare function push(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath, {quiet}: {
+    quiet: any;
+}): void;
 export declare function listUpload(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): void;
-export declare function upload(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): void;
-export declare function fetch(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): void;
+export declare function upload(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath, {quiet}: {
+    quiet: any;
+}): void;
+export declare function fetch(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath, {quiet}: {
+    quiet: any;
+}): void;
 export declare function listDownloadImpl(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): any;
 export declare function listDownload(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): void;
-export declare function download(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath): void;
+export declare function download(rootDir: AbsoluteDirectoryPath, configDir: AbsoluteDirectoryPath, {quiet}: {
+    quiet: any;
+}): void;
