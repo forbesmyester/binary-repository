d33311a58d58f34585c74299a36afba31cb0018f
Matthew Forrester <matthew.forrester@speechmarks.com>
2017-11-05 09:47:27 +0000

    fix: sorting of commits when downloading

--- src/commitFilenameSorter.ts
+++ src/commitFilenameSorter.ts
@@ -1,16 +1,17 @@
 import { BASE_TLID_TIMESTAMP, BASE_TLID_UNIQUENESS, Filename } from './Types';
 import * as getTlidEncoderDecoder from 'get_tlid_encoder_decoder';
+import Client from './Client';
 
 let tlidEncoderDecoder = getTlidEncoderDecoder(BASE_TLID_TIMESTAMP, BASE_TLID_UNIQUENESS);
 
 // TODO: Write tests for this.
 
 export default function(a: Filename, b: Filename): number {
-    let tlidRe = /.*\/(.*)\-.*\.commit$/;
-    let ap = a.path.match(tlidRe);
-    let bp = b.path.match(tlidRe);
-    if ((!ap) || (!bp)) {
-        return 0;
+    let api = Client.infoFromCommitFilename(a.path);
+    let bpi = Client.infoFromCommitFilename(b.path);
+    if ((!api.commitId) || (!bpi.commitId)) {
+        console.log("NO SORT!");
+        throw new Error(`Could not decode a filename ${a} or ${b}`);
     }
-    return tlidEncoderDecoder.sort(ap[1], bp[1]);
+    return tlidEncoderDecoder.sort(api.commitId, bpi.commitId);
 }
