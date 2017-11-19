16ba31eb0fc13ec9287fef4a51b3e9a675a6656c
Matthew Forrester <matthew.forrester@speechmarks.com>
2017-11-19 11:36:21 +0000

    fix: ensure file stat times are rounded to the nearest second

--- src/myStats.ts
+++ src/myStats.ts
@@ -1,8 +1,28 @@
 import { Stats } from 'fs';
+
+function round(d: number): number {
+    return Math.floor(d / 1000) * 1000;
+}
+
 export default function myStat(stat, f: string, cb: (err: null|NodeJS.ErrnoException, stats: null|Stats) => void): void {
     stat(f, (e: null|NodeJS.ErrnoException, s: Stats) => {
-        if (e === null) { return cb(e, s); }
+
+        let ss: Stats = s ? Object.assign({}, s, {
+            atimeMs: (s.atimeMs ? round(s.atimeMs) : null),
+            mtimeMs: (s.mtimeMs ? round(s.mtimeMs) : null),
+            ctimeMs: (s.ctimeMs ? round(s.ctimeMs) : null),
+            birthtimeMs: (s.birthtimeMs ? round(s.birthtimeMs) : null),
+            atime: (s.atime ? new Date(round(s.atime.getTime())) : null),
+            mtime: (s.mtime ? new Date(round(s.mtime.getTime())) : null),
+            ctime: (s.ctime ? new Date(round(s.ctime.getTime())) : null),
+            birthtime: (s.birthtime ? new Date(round(s.birthtime.getTime())) : null)
+        }): s;
+
+        if (e === null) { return cb(e, ss); }
         if (e.code == 'ENOENT') { return cb(null, null); }
+
+
+
         cb(e, null);
     });
 }
